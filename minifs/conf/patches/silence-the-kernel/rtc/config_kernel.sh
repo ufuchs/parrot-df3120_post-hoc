bc30edf8e3ce8b442fa93eb1a00283a8098b2a3c
Uli Fuchs <ufuchs@gmx.com>
2012-08-26 22:20:32 +0200

    deleted:  minifs/conf/patches/silence-the-kernel

--- minifs/conf/patches/silence-the-kernel/rtc/config_kernel.sh
+++
@@ -1,1 +0,0 @@
-diff -Naur ./conf-df3120/config_kernel.conf.minifs ./conf-df3120/config_kernel.conf
--- minifs/conf/patches/silence-the-kernel/s3c2410_udc/diff-s3c2410_udc.sh
+++
@@ -1,1 +0,0 @@
-diff -Naur ./build-df3120/linux.minifs/drivers/usb/gadget/s3c2410_udc.c ./build-df3120/linux/drivers/usb/gadget/s3c2410_udc.c
