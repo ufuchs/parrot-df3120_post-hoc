21cffc36d14b2482260d9530463c2ad2866745a8
Uli Fuchs <ufuchs@gmx.com>
2012-08-27 22:49:16 +0200

    refined: improvments.sh

--- workflow-improvments/improvments.sh
+++ workflow-improvments/improvments.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+CONF_DIR="./conf/"
+export STAGE_DIR=$(pwd)"/stage"
+
+if [ ! -d "$STAGE_DIR" ]; then
+  mkdir "$STAGE_DIR"
+fi
+
+cd "$CONF_DIR"
+
+# should run in loop
+. ./00utils.sh
+. ./01setenv.sh
+. ./02add-df3120-to-etc-hosts.sh
+. ./03hotplug-usb-gadget.sh
+
+cd -
+
+
+
+
