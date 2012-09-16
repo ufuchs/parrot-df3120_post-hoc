221390914a139ccc1b1a05e821387ead24e0008a
Uli Fuchs <ufuchs@gmx.com>
2012-09-16 20:53:08 +0200

    'TOOLCHAIN_HOME' var no longer needed

--- minifs/df3120_minifs_build.sh
+++ minifs/df3120_minifs_build.sh
@@ -11,6 +11,21 @@
 #	PATH		to 'PATH=$PATH:"$TOOLCHAIN_HOME"/bin'
 # there is no longer any need to adjust the environment manually.
 # even reasonable if you have more than one minifs project on your desk.
+#
+###############################################################################
+#
+# Update 2012-09-16
+# ------------------
+# There is no longer any need for a 'TOOLCHAIN_HOME' variable.
+# See
+#   https://sites.google.com/site/repurposelinux/df3120
+# section
+#   Toolchain/rootfs
+#
+# The current version of 'minifs' (esp. in 'conf/arch/armv4t.sh') defines
+# a variable called 'TARGET_FULL_ARCH' which contains this information.
+# On the other hand this variable 'TARGET_FULL_ARCH' is used to populate the
+# 'CROSS_BASE' and 'CROSS' variables in the 'minifs-build.sh'.
 ###############################################################################
 
 BASE="$(pwd)"
@@ -18,12 +33,12 @@ BASE="$(pwd)"
 export MINIFS_BOARD=df3120
 
 # location of the used toolchain
-TOOLCHAIN_HOME="$BASE"/toolchain/arm-v4t-linux-uclibcgnueabi
+# TOOLCHAIN_HOME="$BASE"/toolchain/arm-v4t-linux-uclibcgnueabi
 
 # prevents multiple concatenations
-if [[ $PATH != *"$TOOLCHAIN_HOME"* ]]; then
-  export PATH=$PATH:"$TOOLCHAIN_HOME"/bin
-fi
+# if [[ $PATH != *"$TOOLCHAIN_HOME"* ]]; then
+#   export PATH=$PATH:"$TOOLCHAIN_HOME"/bin
+# fi
 
 # invokes the true 'minifs_build.sh' script
 "$BASE"/minifs_build.sh "$@"
